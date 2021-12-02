a711515d17f4e033c44323249d5c02e5b5894443
natasha80 <61574584+natasha80@users.noreply.github.com>
2021-12-02 20:55:19 +0700

    initial commit

--- src/components/Price.js
+++ src/components/Price.js
@@ -1,17 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types"
 
 const Price = ({ price, code }) => {
     if (code === "USD") {
       return <p className="item-price">${price}</p>;
     } else if (code === "EUR") {
       return <p className="item-price">€{price}</p>;
-    } else {
-      return (
-        <p className="item-price">
-          {price} {code}
-        </p>
-      );
-    }
-  };
+    
+};
+
+Price.propTypes = {
+  price: PropTypes.string,
+  code: PropTypes.string
+}
+
+Price.defaultProps = {
+  price: '0.0',
+  code: 'USD'
+}
 
 export default Price; 
--- src/components/Quantity.js
+++ src/components/Quantity.js
@@ -1,8 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Quantity = ({ quantity }) => {
     const level = quantity <= 10 ? 'level-low' : quantity <= 20 ? 'level-medium' : 'level-high';
         return <p className={`item-quantity ${level}`}>{quantity} left</p>;
 };
+
+Quantity.propTypes = {
+  quantity: PropTypes.number
+}
+
+Quantity.defaultProps = {
+  quantity: 0
+}
 
 export default Quantity;
